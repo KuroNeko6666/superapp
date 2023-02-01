f7a778e5fa3b2673cabeb53b71cfdea80c76b498
“Agung <agungpriyatno666@email.com>
2023-02-01 15:28:35 +0700

    update

--- src/app/views/home/components/sidebar/sidebar-data.ts
+++ src/app/views/home/components/sidebar/sidebar-data.ts
@@ -1,7 +1,7 @@
 export class MENU_DATA {
   public data = [
     {
-      name: 'Dashboard',
+      name: 'Dasbor',
       path: '/home/dashboard',
       icon: 'fa-solid fa-table-columns',
       collapse: true,
